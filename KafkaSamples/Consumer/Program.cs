f660bfbe5d8bff41d6490a16176ca0d107ad322d
Igor Moura <igor.moura254@hotmail.com>
2022-11-23 14:43:43 -0300

    Initial and simple producer/consumer of kafka cluster

---
+++ KafkaSamples/Consumer/Program.cs
@@ -0,0 +1,25 @@
+﻿using Confluent.Kafka;
+
+// Configurações iniciais do Consumer
+var config = new ConsumerConfig
+{
+    // Grupo de consumidores
+    GroupId = "consumers1",
+
+    // BootstrapServers > A URL de um ou mais brokers/servers como ponto de partida
+    // em que fornecemos para que ocorra um fetch/discover inicial de metadata 
+    // como tópicos, particões, etc sobre o cluster kafka
+    // !!! é bom fornecer mais do que um no caso deste estar indisponível !!!
+    BootstrapServers = "localhost:9092"
+};
+
+// Informando qual o tipo de dado da mensagem que será recebido (chave <string> / valor <string>)
+using var consumer = new ConsumerBuilder<string, string>(config).Build();
+
+consumer.Subscribe("topic-test");
+
+while (true)
+{
+    var result = consumer.Consume();
+    Console.Write($"Mensagem: {result.Message.Key}-{result.Message.Value}");
+}
--- KafkaSamples/Producer/Program.cs
+++ KafkaSamples/Producer/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Confluent.Kafka;
+
+// Configurações iniciais do Produtor
+var config = new ProducerConfig
+{
+    // BootstrapServers > A URL de um ou mais brokers/servers como ponto de partida
+    // em que fornecemos para que ocorra um fetch/discover inicial de metadata 
+    // como tópicos, particões, etc sobre o cluster kafka
+    // !!! é bom fornecer mais do que um no caso deste estar indisponível !!!
+    BootstrapServers = "localhost:9092"
+};
+
+// Informando qual o tipo de dado da mensagem que será produzido (chave <string> / valor <string>)
+using var producer = new ProducerBuilder<string, string>(config).Build();
+
+var message = new Message<string, string>
+{
+    //<string>
+    Key = Guid.NewGuid().ToString(),
+    //<string>
+    Value = $"Mensagem teste {DateTime.Now}"
+};
+
+var result = await producer.ProduceAsync("topic-test", message);
+
+Console.Write($"Partition: {result.Partition} Offset: {result.Offset}");
+Console.ReadLine();
