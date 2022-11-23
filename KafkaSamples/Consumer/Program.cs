ce00353ed43cf4b125c7575bdc9b4954947d27ed
Igor Moura <igor.moura254@hotmail.com>
2022-11-23 15:59:13 -0300

    Initial implementation of schema registry

--- KafkaSamples/Consumer/Program.cs
+++ KafkaSamples/Consumer/Program.cs
@@ -1,4 +1,18 @@
 ﻿using Confluent.Kafka;
+using Confluent.Kafka.SyncOverAsync;
+using Confluent.SchemaRegistry;
+using Confluent.SchemaRegistry.Serdes;
+using Schemas;
+
+// Configurações iniciais do Schema Registry
+var schemaConfig = new SchemaRegistryConfig
+{
+    Url = "http://localhost:8081"
+};
+
+// O cliente irá bater no Schema Registry e irá salvar
+// os schemas em um cache, para evitar de ficar buscando toda hora
+var schemaRegistry = new CachedSchemaRegistryClient(schemaConfig);
 
 // Configurações iniciais do Consumer
 var config = new ConsumerConfig
@@ -13,13 +27,15 @@ var config = new ConsumerConfig
     BootstrapServers = "localhost:9092"
 };
 
-// Informando qual o tipo de dado da mensagem que será recebido (chave <string> / valor <string>)
-using var consumer = new ConsumerBuilder<string, string>(config).Build();
+// Informando qual o tipo de dado da mensagem que será recebido (chave <string> / valor <Curso>)
+using var consumer = new ConsumerBuilder<string, Curso>(config)
+    .SetValueDeserializer(new AvroDeserializer<Curso>(schemaRegistry).AsSyncOverAsync())
+    .Build();
 
-consumer.Subscribe("topic-test");
+consumer.Subscribe("cursos");
 
 while (true)
 {
     var result = consumer.Consume();
-    Console.Write($"Mensagem: {result.Message.Key}-{result.Message.Value}");
+    Console.Write($"Mensagem: {result.Message.Key}-{result.Message.Value.Id}, {result.Message.Value.Descricao}");
 }
--- KafkaSamples/Producer/Program.cs
+++ KafkaSamples/Producer/Program.cs
@@ -1,4 +1,17 @@
 ﻿using Confluent.Kafka;
+using Confluent.SchemaRegistry;
+using Confluent.SchemaRegistry.Serdes;
+using Schemas;
+
+// Configurações iniciais do Schema Registry
+var schemaConfig = new SchemaRegistryConfig
+{
+    Url = "http://localhost:8081"
+};
+
+// O cliente irá bater no Schema Registry e irá salvar
+// os schemas em um cache, para evitar de ficar buscando toda hora
+var schemaRegistry = new CachedSchemaRegistryClient(schemaConfig);
 
 // Configurações iniciais do Produtor
 var config = new ProducerConfig
@@ -10,18 +23,24 @@ var config = new ProducerConfig
     BootstrapServers = "localhost:9092"
 };
 
-// Informando qual o tipo de dado da mensagem que será produzido (chave <string> / valor <string>)
-using var producer = new ProducerBuilder<string, string>(config).Build();
+// Informando qual o tipo de dado da mensagem que será produzido (chave <string> / valor <Curso>)
+using var producer = new ProducerBuilder<string, Curso>(config)
+    .SetValueSerializer(new AvroSerializer<Curso>(schemaRegistry))
+    .Build();
 
-var message = new Message<string, string>
+var message = new Message<string, Curso>
 {
     //<string>
     Key = Guid.NewGuid().ToString(),
-    //<string>
-    Value = $"Mensagem teste {DateTime.Now}"
+    //<Curso>
+    Value = new Curso()
+    {
+        Id = new Guid().ToString(),
+        Descricao = "Curso de Apache Kafka"
+    }
 };
 
-var result = await producer.ProduceAsync("topic-test", message);
+var result = await producer.ProduceAsync("cursos", message);
 
 Console.Write($"Partition: {result.Partition} Offset: {result.Offset}");
 Console.ReadLine();
