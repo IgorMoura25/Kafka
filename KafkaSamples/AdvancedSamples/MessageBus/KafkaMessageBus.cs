27d0e89952ec01cb5c453b9a638b527f1bf01e46
Igor Moura <igor.moura254@hotmail.com>
2022-11-24 19:47:20 -0300

    Transactions with kafka

--- KafkaSamples/AdvancedSamples/MessageBus/KafkaMessageBus.cs
+++ KafkaSamples/AdvancedSamples/MessageBus/KafkaMessageBus.cs
@@ -21,6 +21,7 @@ namespace MessageBus
                 Acks = Acks.All, // Para garantir a idempotência (sem duplicatas)
                 MaxInFlight = 1, // Determina quantidade de conexões com o kafka
                 MessageSendMaxRetries = 2, // Tenta enviar novamente no máximo 2 vezes
+                TransactionalId = Guid.NewGuid().ToString(),
                 BootstrapServers = _bootstrapServers
             };
 
@@ -30,11 +31,29 @@ namespace MessageBus
                 .SetValueSerializer(new CustomSerializer<T>())
                 .Build();
 
+            // Irá se comunicar com o coordenador de transações
+            // através do Transaction Id definido acima
+            // e vou começar um processo de transação, ou seja,
+            // irei enviar vários eventos mas só no final
+            // vou confirmar a transação se tudo ocorrer bem
+            // dai você pode enviar todas as mensagens
+            producer.InitTransactions(TimeSpan.FromSeconds(5));
+
+            producer.BeginTransaction();
+
+            // Envio mensagem 1, envio mensagem 2, etc...
+
+            // Aborto se deu algo errado
+            //producer.AbortTransaction();
+
             var result = await producer.ProduceAsync(topic, new Message<string, T>
             {
                 Key = Guid.NewGuid().ToString(),
                 Value = message
             });
+
+            // Commito se deu tudo certo
+            producer.CommitTransaction();
 
             await Task.CompletedTask;
         }
@@ -52,8 +71,9 @@ namespace MessageBus
                     //AutoOffsetReset = AutoOffsetReset.Earliest, // Consuma todas as mensagens represadas que estão no tópico
                     EnableAutoCommit = false, // false == A aplicação que se encarregará de dizer que "LEU" a mensagem do kafka
                     EnablePartitionEof = true, // true == O kafka avisa através da "" se a partição chegou ao fim
-                    EnableAutoOffsetStore = false // false == O offset da mensagem lida não será deslocado automaticamente,
-                                                  // cabendo ao consumidor fazer isso manualmente
+                    EnableAutoOffsetStore = false, // false == O offset da mensagem lida não será deslocado automaticamente,
+                                                   // cabendo ao consumidor fazer isso manualmente
+                    IsolationLevel = IsolationLevel.ReadCommitted // Para ler somente mensagens que foram confirmadas pela transação
                 };
 
                 using var consumer = new ConsumerBuilder<string, T>(config)
